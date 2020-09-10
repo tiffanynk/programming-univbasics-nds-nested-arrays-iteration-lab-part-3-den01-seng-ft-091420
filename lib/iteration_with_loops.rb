edf62715d8798b4b1bd059c943490c49fc9bda3f
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-10 17:03:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,7 +12,7 @@ def join_nested_strings(src)
       if src[row_index][element_index].class == Strings
         nested_strings += src[row_index][element_index]
       end
-      
+
       element_index += 1
     end
     row_index += 1
